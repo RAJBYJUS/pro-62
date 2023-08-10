ca88c9c8b0a90e00b4e19c0073cd2643d5991180
RAJBYJUS <111908923+RAJBYJUS@users.noreply.github.com>
2023-08-10 17:52:43 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const  firebaseConfig = {
+  apiKey: "AIzaSyB133VHLyuxF3icwNedzQ3mUJt03lJOcPs",
+  authDomain: "storytelling-78ce9.firebaseapp.com",
+  databaseURL: "https://storytelling-78ce9-default-rtdb.firebaseio.com",
+  projectId: "storytelling-78ce9",
+  storageBucket: "storytelling-78ce9.appspot.com",
+  messagingSenderId: "678067596906",
+  appId: "1:678067596906:web:99006986cc44a971ac2413"
+};
